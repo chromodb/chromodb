f67ab262bef5526cba9c78a09a4fecd2af2cd714
Alex Gaetano Padula <apadula@cursusdb.com>
2024-01-23 16:35:05 -0500

    fractal tree implementation for database structure with all required functionalities tested interally, tests will follow

--- datastructure/datastructure.go
+++ datastructure/datastructure.go
@@ -20,6 +20,10 @@
 package datastructure
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
 	"os"
 )
 
@@ -28,4 +32,281 @@ type FractalTree struct {
 	dataFile   *os.File
 	indexFile  *os.File
 	nextOffset int64
+}
+
+// Delete takes a provided key and deletes the entry
+func (db *FractalTree) Delete(key []byte) error {
+	// Reset the offset of the index file to the beginning
+	_, err := db.indexFile.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	// Initialize a buffer to store the updated index data
+	var updatedIndexBuffer bytes.Buffer
+
+	for {
+		// Read key from the index file
+		indexKey := make([]byte, len(key))
+		if _, err := db.indexFile.Read(indexKey); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		// Compare keys
+		if bytes.Equal(indexKey, key) {
+			// Read and discard the offset (we don't need it for deletion)
+			if _, err := db.indexFile.Seek(int64(binary.Size(int64(0))), io.SeekCurrent); err != nil {
+				return err
+			}
+
+			// Skip the data record in the data file
+			var keyLength, valueLength uint32
+			if err := binary.Read(db.dataFile, binary.LittleEndian, &keyLength); err != nil {
+				return err
+			}
+			if err := binary.Read(db.dataFile, binary.LittleEndian, &valueLength); err != nil {
+				return err
+			}
+			dataRecordSize := int64(binary.Size(keyLength)) + int64(binary.Size(valueLength)) + int64(keyLength) + int64(valueLength) + int64(binary.Size(int64(0)))
+			if _, err := db.dataFile.Seek(dataRecordSize, io.SeekCurrent); err != nil {
+				return err
+			}
+		} else {
+			// Write the key to the updated index buffer
+			if _, err := updatedIndexBuffer.Write(indexKey); err != nil {
+				return err
+			}
+
+			// Read and write the offset to the updated index buffer
+			var offset int64
+			if err := binary.Read(db.indexFile, binary.LittleEndian, &offset); err != nil {
+				return err
+			}
+			if err := binary.Write(&updatedIndexBuffer, binary.LittleEndian, offset); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Truncate the index file to remove the deleted key
+	if err := db.indexFile.Truncate(0); err != nil {
+		return err
+	}
+
+	// Write the updated index data to the index file
+	if _, err := db.indexFile.Write(updatedIndexBuffer.Bytes()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// OpenFractalTree opens or creates a FractalTree
+func OpenFractalTree(dataFilename, indexFilename string) (*FractalTree, error) {
+	dataFile, err := os.OpenFile(dataFilename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	indexFile, err := os.OpenFile(indexFilename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	// Calculate the next offset
+	dataFileInfo, err := dataFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+	nextOffset := dataFileInfo.Size()
+
+	return &FractalTree{
+		dataFile:   dataFile,
+		indexFile:  indexFile,
+		nextOffset: nextOffset,
+	}, nil
+}
+
+// Close closes the FractalTree
+func (db *FractalTree) Close() error {
+	if err := db.dataFile.Close(); err != nil {
+		return err
+	}
+	return db.indexFile.Close()
+}
+
+// Put is like insert & update.  Will create a key-value but will replace an existing
+// if key already exists
+func (db *FractalTree) Put(key, value []byte) error {
+	// Reset the offset of the index file to the beginning
+	_, err := db.indexFile.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	// Check if the key already exists
+	for {
+		// Read key from the index file
+		indexKey := make([]byte, len(key))
+		if _, err := db.indexFile.Read(indexKey); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		// Compare keys
+		if bytes.Equal(indexKey, key) {
+			// Key already exists, update the value
+
+			// Read offset from the index file
+			var offset int64
+			if err := binary.Read(db.indexFile, binary.LittleEndian, &offset); err != nil {
+				return err
+			}
+
+			// Seek to the corresponding offset in the data file
+			_, err := db.dataFile.Seek(offset, io.SeekStart)
+			if err != nil {
+				return err
+			}
+
+			// Update the value in the data file
+			if err := db.writeDataRecord(db.dataFile, offset, key, value); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		// Read and discard the offset (we don't need it for lookup)
+		if _, err := db.indexFile.Seek(int64(binary.Size(int64(0))), io.SeekCurrent); err != nil {
+			return err
+		}
+	}
+
+	// Key does not exist, proceed with adding the new key-value pair
+
+	// Write key to the index file
+	if _, err := db.indexFile.Write(key); err != nil {
+		return err
+	}
+
+	// Get the current offset in the data file
+	offset, err := db.dataFile.Seek(0, io.SeekEnd)
+	if err != nil {
+		return err
+	}
+
+	// Write offset to the index file
+	if err := binary.Write(db.indexFile, binary.LittleEndian, offset); err != nil {
+		return err
+	}
+
+	// Write key-value pair to the data file
+	// (using the offset obtained before writing to the index file)
+	if err := db.writeDataRecord(db.dataFile, offset, key, value); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// writeDataRecord writes a key-value record to the specified data file at the specified offset
+func (db *FractalTree) writeDataRecord(dataFile io.Writer, offset int64, key, value []byte) error {
+	// Write key length
+	if err := binary.Write(dataFile, binary.LittleEndian, uint32(len(key))); err != nil {
+		return err
+	}
+
+	// Write value length
+	if err := binary.Write(dataFile, binary.LittleEndian, uint32(len(value))); err != nil {
+		return err
+	}
+
+	// Write key
+	if _, err := dataFile.Write(key); err != nil {
+		return err
+	}
+
+	// Write value
+	if _, err := dataFile.Write(value); err != nil {
+		return err
+	}
+
+	// Write offset of the next record in the data file
+	if err := binary.Write(dataFile, binary.LittleEndian, offset); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get retrieves the value associated with a key
+func (db *FractalTree) Get(key []byte) ([]byte, error) {
+	// Reset the offset of the index file to the beginning
+	_, err := db.indexFile.Seek(0, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		// Read key from the index file
+		indexKey := make([]byte, len(key))
+		if _, err := db.indexFile.Read(indexKey); err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		// Compare keys
+		if bytes.Equal(indexKey, key) {
+			// Read offset from the index file
+			var offset int64
+			if err := binary.Read(db.indexFile, binary.LittleEndian, &offset); err != nil {
+				return nil, err
+			}
+
+			// Seek to the corresponding offset in the data file
+			_, err := db.dataFile.Seek(offset, io.SeekStart)
+			if err != nil {
+				return nil, err
+			}
+
+			// Read the key length
+			var keyLength uint32
+			if err := binary.Read(db.dataFile, binary.LittleEndian, &keyLength); err != nil {
+				return nil, err
+			}
+
+			// Read the value length
+			var valueLength uint32
+			if err := binary.Read(db.dataFile, binary.LittleEndian, &valueLength); err != nil {
+				return nil, err
+			}
+
+			// Read the key
+			keyData := make([]byte, keyLength)
+			if _, err := db.dataFile.Read(keyData); err != nil {
+				return nil, err
+			}
+
+			// Read the value
+			valueData := make([]byte, valueLength)
+			if _, err := db.dataFile.Read(valueData); err != nil {
+				return nil, err
+			}
+
+			return valueData, nil
+		}
+
+		// Read and discard the offset (we don't need it for lookup)
+		if _, err := db.indexFile.Seek(int64(binary.Size(int64(0))), io.SeekCurrent); err != nil {
+			return nil, err
+		}
+	}
+
+	// Key not found
+	return nil, fmt.Errorf("key not found")
 }
