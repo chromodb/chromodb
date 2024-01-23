e3e042984f19e210025e9aef57b98b38b117fb7c
Alex Gaetano Padula <apadula@cursusdb.com>
2024-01-23 16:48:16 -0500

    finish up main and system!

--- main.go
+++ main.go
@@ -19,6 +19,120 @@
  */
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"chromodb/datastructure"
+	"chromodb/system"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// System starts here
+// ./chromodb
+// ./chromodb --help
+// ./chromodb --memory-limit=7500 * 1024 * 1024
+// ./chromodb --shell=false --user=alex --pasword=somepassword
+// ./chromodb --shell=false --user=alex --pasword=somepassword --tls=true --key="key.pem" --cert="cert.pem"
 func main() {
-	// coming
+	var db system.Database // Main system variable
+
+	db.Config.MemoryLimit = 750 * 1024 * 1024 // 750MB
+
+	go db.MonitorMemory() // This is for the MEM/mem command.  We check every 5 seconds
+
+	// Load database and index file
+	tree, err := datastructure.OpenFractalTree("chromo.db", "chromo.idx")
+	if err != nil {
+		return
+	}
+
+	db.FractalTree = tree // Set tree into system variable
+	db.Config.Port = 7676 // Set default port
+
+	var shell bool  // Use shell, good for embedded stuff
+	var tls bool    // Upgrade clients to TLS
+	var help bool   // Help show all flags
+	var user string // Database user for remote connections to use
+	var pass string // Database password for remote connections to use
+	// if tls enabled
+	var cert string // tls cert location
+	var key string  // tls key location
+
+	flag.BoolVar(&help, "help", help, "displays flag instructions")
+	flag.BoolVar(&shell, "shell", shell, "true or false to use internal shell")
+	flag.BoolVar(&tls, "tls", tls, "enable tls listener.  you must provide a cert and key using --cert and --key flags.")
+	flag.IntVar(&db.Config.MemoryLimit, "memory-limit", db.Config.MemoryLimit, "configure desired memory limit.  default is 750mb i.e 750 * 1024 * 1024")
+	flag.StringVar(&user, "user", user, "database user username for when using network")
+	flag.StringVar(&pass, "pass", pass, "database user password for when using network")
+	flag.StringVar(&cert, "key", user, "tls cert location")
+	flag.StringVar(&key, "cert", pass, "tls key location")
+	flag.IntVar(&db.Config.Port, "port", db.Config.Port, "tcp/tls listener port default is 7676")
+
+	flag.Parse() // parse flags
+
+	if help { // if help display flag usages
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if !shell { // if not shell we will start up a networked ChromoDB
+		if user == "" && pass == "" {
+			fmt.Println("Database username and password is required when configuring database to be networked.")
+			os.Exit(1)
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		go func() {
+			sigCh := make(chan os.Signal, 1)
+			signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+			<-sigCh
+			cancel()
+		}()
+
+		db.StartTCPTLSListener(ctx)
+
+		db.Stop()
+
+	} else { // Not networked
+
+		qChan := make(chan []byte) // Query channel
+
+		go func() {
+			for {
+
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print("db>")
+				query, _ := reader.ReadBytes('\n')
+
+				qChan <- bytes.TrimSpace(query) // Receive from standard input and relay to query channel
+			}
+		}()
+
+		for {
+			query := <-qChan // Receive from query channel
+
+			// Check for exit command
+			if bytes.HasSuffix(query, []byte("exit")) {
+				fmt.Println("..\nbye!")
+				break
+			}
+
+			// Execute the command
+			output, err := db.ExecuteCommand(query)
+			if err != nil {
+				fmt.Println("Error executing query:", err)
+				continue
+			}
+
+			// Print the output of the command
+			fmt.Println(output)
+		}
+	}
 }
--- system/system.go
+++ system/system.go
@@ -20,9 +20,18 @@
 package system
 
 import (
+	"bufio"
+	"bytes"
 	"chromodb/datastructure"
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
 	"net"
+	"os"
+	"runtime"
 	"sync"
+	"time"
 )
 
 // Database is the ChromoDB main struct
@@ -41,4 +50,181 @@ type Config struct {
 	TLS         bool   // Whether listener should listen on TLS or not
 	TLSKey      string // If TLS is set where is the TLS key located?
 	TLSCert     string // if TLS is set where is TLS cert located?
+}
+
+func (db *Database) MonitorMemory() {
+	ticker := time.NewTicker(time.Second * 5) // Check memory usage every 5 seconds
+
+	for range ticker.C {
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+
+		db.CurrentMemoryUsage = int(m.Alloc)
+		if m.Alloc > uint64(db.Config.MemoryLimit) {
+			fmt.Println("Memory usage exceeds the limit. Exiting...")
+			os.Exit(1)
+		}
+	}
+}
+
+func (db *Database) ExecuteCommand(query []byte) (interface{}, error) {
+	res, err := db.QueryParser(query)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (db *Database) QueryParser(query []byte) (interface{}, error) {
+	switch {
+	case bytes.HasPrefix(bytes.ToUpper(query), []byte("MEM")):
+		return []byte(fmt.Sprintf("Current memory usage: %d bytes", db.CurrentMemoryUsage)), nil
+	case bytes.HasPrefix(bytes.ToUpper(query), []byte("PUT")):
+		opSpl := bytes.Split(query, []byte("->"))
+
+		if len(opSpl) != 3 {
+			return nil, errors.New("bad sequence")
+		}
+
+		err := db.FractalTree.Put(bytes.TrimSpace(opSpl[1]), bytes.TrimSpace(opSpl[2]))
+		if err != nil {
+			return nil, err
+		}
+
+		return []byte("PUT SUCCESS"), nil
+	case bytes.HasPrefix(bytes.ToUpper(query), []byte("GET")):
+		opSpl := bytes.Split(query, []byte("->"))
+
+		if len(opSpl) < 2 {
+			return nil, errors.New("bad sequence")
+		}
+
+		res, err := db.FractalTree.Get(bytes.TrimSpace(opSpl[1]))
+		if err != nil {
+			return nil, err
+		}
+
+		return res, nil
+	case bytes.HasPrefix(bytes.ToUpper(query), []byte("DEL")):
+		opSpl := bytes.Split(query, []byte("->"))
+
+		if len(opSpl) < 2 {
+			return nil, errors.New("bad sequence")
+		}
+
+		db.FractalTree.Delete(bytes.TrimSpace(opSpl[1]))
+
+		return []byte("DEL SUCCESS"), nil
+	}
+
+	return nil, errors.New("nonexistent command")
+}
+
+func (db *Database) StartTCPTLSListener(ctx context.Context) error {
+	db.Wg = &sync.WaitGroup{}
+	addr := "0.0.0.0:7676" // is the default for ChromoDB
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	db.TCPListener = listener
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				// Handle errors (e.g., when the server is shutting down)
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					fmt.Println("Error accepting connection:", err)
+				}
+				continue
+			}
+
+			// Handle the connection in a separate goroutine
+			db.Wg.Add(1)
+			go func(c net.Conn) {
+				defer db.Wg.Done()
+				defer c.Close()
+
+				if db.Config.TLS {
+					cert, err := tls.LoadX509KeyPair(db.Config.TLSCert, db.Config.TLSKey)
+					if err != nil {
+						fmt.Println("Error loading certificate and private key:", err)
+						return
+					}
+
+					// Perform TLS handshake to upgrade the connection
+					tlsConn := tls.Server(conn, &tls.Config{
+						Certificates:       []tls.Certificate{cert},
+						InsecureSkipVerify: false,
+					})
+
+					err = tlsConn.Handshake()
+					if err != nil {
+						fmt.Println("TLS handshake error:", err)
+						return
+					}
+				}
+
+				// HANDLE AUTH
+
+				reader := bufio.NewReader(conn)
+
+				for {
+					// Read a line (until CRLF)
+					line, err := reader.ReadBytes('\n')
+					if err != nil {
+						fmt.Println("Error reading from connection:", err)
+						return
+					}
+
+					// Check for trailing CRLF
+					if len(line) >= 2 && line[len(line)-2] == '\r' && line[len(line)-1] == '\n' {
+						// Trailing CRLF found
+						res, err := db.QueryParser(line)
+						if err != nil {
+							conn.Write(append([]byte(err.Error()), []byte("\r\n")...))
+						} else {
+							conn.Write(append(res.([]byte), []byte("\r\n")...))
+						}
+					} else if line[len(line)-1] == '\n' {
+						// Trailing LF found
+						res, err := db.QueryParser(line)
+						if err != nil {
+							conn.Write(append([]byte(err.Error()), []byte("\r\n")...))
+						} else {
+							conn.Write(append(res.([]byte), []byte("\r\n")...))
+						}
+					} else {
+						fmt.Println("Invalid format, missing trailing CRLF")
+					}
+				}
+
+			}(conn)
+		}
+	}()
+
+	fmt.Println("TCP/TLS listener is listening on", addr)
+
+	// Wait for the shutdown signal
+	select {
+	case <-ctx.Done():
+		return nil
+	}
+}
+
+// Stop stops the TCP server
+func (db *Database) Stop() {
+	fmt.Println("TCP/TLS listener is shutting down...")
+	if db.TCPListener != nil {
+		_ = db.TCPListener.Close()
+	}
+	// Wait for all active connections to finish
+	db.Wg.Wait()
+	fmt.Println("TCP/TLS listener stopped")
 }
